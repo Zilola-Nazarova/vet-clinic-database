1c2e42840aeb0b48f3076e31e79f2434ec99991e
zilola <zilolanazarova1994@gmail.com>
2023-09-18 11:30:57 +0500

    Fix typo (lowercase the name)

--- queries.sql
+++ queries.sql
@@ -1,6 +1,6 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * FROM animals WHERE NAME LIKE '%mon';
+SELECT * FROM animals WHERE name LIKE '%mon';
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 SELECT name FROM animals WHERE (neutered = true) AND (escape_attempts < 3);
 SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
