2d15562d203909def6d38e4bab12906c443007db
zilola <zilolanazarova1994@gmail.com>
2023-09-18 14:30:52 +0500

    User aggregate functions and GROUP BY to get summarized data

--- queries.sql
+++ queries.sql
@@ -84,3 +84,21 @@ COMMIT;
 -- verify that changes persist
 SELECT name, weight_kg
 FROM animals;
+
+
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT neutered, SUM(escape_attempts)
+FROM animals
+GROUP BY neutered;
+
+SELECT species, MIN(weight_kg), MAX(weight_kg)
+FROM animals
+GROUP BY species;
+
+SELECT species, AVG(escape_attempts)
+FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
