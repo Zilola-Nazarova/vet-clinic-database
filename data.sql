e18e23ee8f4d8ee7bf57da976a5552fbccbd3de1
zilola <zilolanazarova1994@gmail.com>
2023-09-19 09:10:36 +0500

    Add data to species and owners tables

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
 
+/* PROJECT 1 */
 INSERT INTO animals
 (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES
 ('Agumon', '2020-02-03', 0, true, 10.23),
@@ -7,6 +8,8 @@ INSERT INTO animals
 ('Pikachu', '2021-01-07', 1, false, 15.04),
 ('Devimon', '2017-05-12', 5, true, 11.00);
 
+
+/* PROJECT 2 */
 INSERT INTO
 animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES
@@ -17,3 +20,23 @@ VALUES
 ('Boarmon', '2005-06-07', 7, true, 20.40),
 ('Blossom', '1998-10-13', 3, true, 17.00),
 ('Ditto', '2022-05-14', 4, true, 22.00);
+
+
+/* PROJECT 3 */
+-- add rows to species table
+INSERT INTO species (name)
+VALUES
+('Pokemon'),
+('Digimon');
+-- add rows to owners table
+INSERT INTO owners (full_name, age)
+VALUES
+('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
+--verify
+SELECT * FROM owners;
+SELECT * FROM species;
