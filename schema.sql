bd9860e2ad23c800428b71eb6192cd322d9c260c
zilola <zilolanazarova1994@gmail.com>
2023-09-19 09:09:11 +0500

    Create new species and owners talbes

--- schema.sql
+++ schema.sql
@@ -1,5 +1,6 @@
 /* Database schema to keep the structure of entire database. */
 
+/* PROJECT 1 */
 CREATE TABLE animals(
     id INT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(250),
@@ -10,5 +11,26 @@ CREATE TABLE animals(
     PRIMARY KEY(id)
 );
 
+/* PROJECT 2 */
 ALTER TABLE animals
 ADD species varchar(250);
+
+
+/* PROJECT 3 */
+-- create owners table
+CREATE TABLE owners(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR(250),
+    age INT,
+    PRIMARY KEY(id)
+);
+-- create species table
+CREATE TABLE species(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(250),
+    PRIMARY KEY(id)
+);
+-- verify
+\d
+SELECT * FROM owners;
+SELECT * FROM species;
